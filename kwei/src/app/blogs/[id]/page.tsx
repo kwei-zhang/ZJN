e6faa7c017bfd93f918712ac162fb10c67185443
Kaiwei Zhang <134007383+KevinZJN@users.noreply.github.com>
2024-12-20 11:09:46 -0500

    fix params

--- kwei/src/app/blogs/[id]/page.tsx
+++ kwei/src/app/blogs/[id]/page.tsx
@@ -1,69 +1,17 @@
-"use client"
 
-import { BlogBadge } from "@/components/blogs/blogBadge";
-import { MotionWrapperVertical } from "@/components/motion";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Separator } from "@/components/ui/separator";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
 
-export default function BlogPage() {
-    const { id } = useParams();
-    const [blog, setBlog] = useState<any>(null);
-    const [loading, setLoading] = useState(true);
+export default async function BlogPage(
+    { params }: { params: { id: string } }
+) {
+    console.log(params.id);
 
-    useEffect(() => {
-        async function fetchBlog() {
-            if (!id || typeof id !== 'string') return;
-            try {
-                const response = await fetch(`/api/blogs/${id}`);
-                const data = await response.json();
-                setBlog(data);
-            } catch (error) {
-                console.error('Error fetching blog:', error);
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        fetchBlog();
-    }, [id]);
-
-    if (!id || typeof id !== 'string') {
-        return <div>Blog not found</div>;
-    }
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+    const blog = await fetch(`${apiUrl}/api/blogs/${params.id}`);
+    const data = await blog.json();
     return (
-        <>
-        <MotionWrapperVertical>
-            <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-                Blogs
-            </h2>
-            <p className="text-muted-foreground">
-                A collection of my thoughts and experiences.
-            </p>
-            
-        </MotionWrapperVertical>
-
-        <Separator className="w-10/12 mt-4" />
-
-        <MotionWrapperVertical delay={0.6} className="w-full flex flex-col items-center">
-            <div className="w-10/12 my-4 flex flex-row justify-center">
-                <Input placeholder="Search" className=" mr-4" />
-                <Button>Search</Button>
-            </div>
-            <BlogBadge />
-        </MotionWrapperVertical>
-
-        <MotionWrapperVertical delay={0.8} className="w-full">
-            <h1>{blog.title}</h1>
-            <p>{blog.content}</p>
-        </MotionWrapperVertical>
-        </>
-    );
+        <div>
+            <h1>Blog Page {params.id}</h1>
+            <p>{data.content}</p>
+        </div>
+    )
 }
